21a24de9ee0222efafb801ee1e6aea019d479218
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-18 16:52:21 +0700

    deklarasi-array-associatve

---
+++ belajar-6/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    Array asosiatif adalah jenis array di PHP di mana setiap elemen array memiliki kunci 
+yang ditentukan oleh pengguna, bukan hanya indeks numerik seperti pada array biasa. 
+Dalam array asosiatif, kunci tersebut dapat berupa string atau bilangan bulat.
+
+    Cara penggunaan array asosiatif mirip dengan array biasa, kecuali bahwa Anda menggunakan kunci 
+untuk mengakses dan menetapkan nilai, bukan menggunakan indeks numerik. 
+*/
+
+// Deklarasi array associative
+$person1 = array(
+    'name' => 'John',
+    'age' => 30,
+    'city' => 'New York'
+);
+// atau
+$person2 = [
+    'name' => 'John',
+    'age' => 30,
+    'city' => 'New York'
+];
