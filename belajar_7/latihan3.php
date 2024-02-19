244c36c1eabc1e74fc14db9d5182bb533ec9cb58
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-19 18:20:14 +0700

    post data

---
+++ belajar_7/latihan3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="latihan4.php" method="post">
+        <label for="nama">
+            Masukan nama :
+            <input type="text" name="nama">
+        </label>
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+
+</html>
