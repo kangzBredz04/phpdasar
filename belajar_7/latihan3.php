25c9a9a6017ab8ebce1e1976e5d4f7e8b96fedbf
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-19 18:25:57 +0700

    post data halaman sendiri

--- belajar_7/latihan3.php
+++ belajar_7/latihan3.php
@@ -8,7 +8,19 @@
 </head>
 
 <body>
-    <form action="latihan4.php" method="post">
+    <!-- Kondisi 1 jika $_POST['nama] belum ada -->
+    <!-- <h1>
+        Hai
+        <?= isset($_POST['nama']) ? $_POST['nama'] : "Admin!" ?>
+    </h1> -->
+
+    <!-- Kondisi 2 jika $_POST['nama] belum ada -->
+    <?php if (isset($_POST['submit'])): ?>
+        <h1>Hallo selamat datang
+            <?= $_POST['nama']; ?>
+        </h1>
+    <?php endif; ?>
+    <form method="post">
         <label for="nama">
             Masukan nama :
             <input type="text" name="nama">
