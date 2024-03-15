2529bb4bad8bbc3f3456db3636e792a916f05652
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-15 11:00:59 +0700

    Menampilkan isi array

--- CodersIndonesia/BelajarPHPDasar1/latihanphp/array.php
+++ CodersIndonesia/BelajarPHPDasar1/latihanphp/array.php
@@ -14,3 +14,21 @@ $makanan = ["Mie", "Nasi Goreng", "Martabak"];
 $anggota[0] = "Wahyu";
 $anggota[1] = "Dimas";
 $anggota[2] = "Wabredz";
+
+// Menampilkan isi array
+// Cara simpel (kurang efektif)
+echo $anggota[0] . "<br>";
+echo $anggota[1] . "<br>";
+echo $anggota[2] . "<br>";
+
+echo "<hr>";
+// Menggunakan perulangan for
+for ($i = 0; $i < count($makanan); $i++) {
+    echo $makanan[$i] . "<br>";
+}
+
+echo "<hr>";
+// Menggunakan perulangan foreach
+foreach ($makanan as $m) {
+    echo $m . "<br>";
+}
