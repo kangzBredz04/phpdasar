a7f655bd8cba0f933089bda56c7aafcd5691aafb
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-15 10:52:32 +0700

    Membuat array dengan berbagai cara

--- CodersIndonesia/BelajarPHPDasar1/latihanphp/array.php
+++ CodersIndonesia/BelajarPHPDasar1/latihanphp/array.php
@@ -1,3 +1,16 @@
 <?php
 // Array adalah salah satu struktur data yang berisi sekumpulan data dan memiliki indeks. 
 // Indeks digunakan untuk mengakses nilai array.
+
+// Membuat array kosong 
+$buah = array();
+$sayuran = [];
+
+// Membuat array sekaligus dengan isinya
+$minuman = array("Kopi", "Teh", "Susu");
+$makanan = ["Mie", "Nasi Goreng", "Martabak"];
+
+// Membuat array melalui indeks tertentu
+$anggota[0] = "Wahyu";
+$anggota[1] = "Dimas";
+$anggota[2] = "Wabredz";
