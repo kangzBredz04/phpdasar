36ddd9c0773a9244cdf84ad0749871a522567d7c
kangzBredz04 <wabredz1234@gmail.com>
2024-09-15 17:24:26 +0700

    tambah file untuk koneksi database

---
+++ Web_Programming_UNPAS/belajar_9/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+// Koneksi ke database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phpdasar";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
